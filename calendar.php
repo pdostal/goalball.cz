f5255e1bbec5c115cc587c11550ac0dd8c511d34
pdostal <git@pdostal.cz>
2013-10-29 08:20:29 +0100

    Table body border.

--- calendar.php
+++ calendar.php
@@ -11,9 +11,9 @@
 		<div class="content">
 <? fse_print_events(array(
 	'number' => 10,
-	'before' => "<table id=\"calendar\">\n<tr class=\"calendar_header\"><th class=\"calendar_date\">Datum</th><th class=\"calendar_category\">Kategorie</th><th class=\"calendar_location\">Místo</th><th class=\"calendar_event\">Událost</th></tr>\n",
-	'after' => "</table>\n",
-	'template' => "<tr><td class=\"calendar_date\">{event_startdate; fmt=\"d.m.Y\";}{event_enddate; fmt=\" - d.m.Y\";}</td><td class=\"calendar_category\">{event_categories}</td><td class=\"calendar_location\">{event_location}</td><td class=\"calendar_event\">{event_subject}</td></tr>\n"
+	'before' => "<table id=\"calendar\">\n<thead><tr class=\"calendar_headrow\"><th class=\"calendar_date\">Datum</th><th class=\"calendar_category\">Kategorie</th><th class=\"calendar_location\">Místo</th><th class=\"calendar_event\">Událost</th></tr></thead>\n<tbody>\n",
+	'after' => "</tbody\n></table>\n",
+	'template' => "<tr class=\"calendar_bodyrow\"><td class=\"calendar_date\">{event_startdate; fmt=\"d.m.Y\";}{event_enddate; fmt=\" - d.m.Y\";}</td><td class=\"calendar_category\">{event_categories}</td><td class=\"calendar_location\">{event_location}</td><td class=\"calendar_event\">{event_subject}</td></tr>\n"
 )); ?>
 		</div>
 	</article>
