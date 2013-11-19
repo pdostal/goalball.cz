9e90850baeff5937ba2e91e0d2f377d23bbc6767
pdostal <git@pdostal.cz>
2013-11-19 10:53:14 +0100

    Update footer

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 		<footer>
 			<span>
 				<a href="<?php bloginfo('url'); ?>/wp-admin/">Administrace</a> |
-				Redakční systém <a href="http://wordpress.org/">Wordpress</a>
-				&copy; <a href="mailto:pdostal@pdostal.cz">Pavel Dostál</a>
+				Redakční systém <a href="http://wordpress.org/">Wordpress</a> |
+				Design vytvořil <a href="http://pdostal.cz/">Pavel Dostál</a>
 			</span>
 			<?php wp_footer(); ?>
 		</footer>
