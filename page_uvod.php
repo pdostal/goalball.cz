ebbd364f59c9524964c212a530b036db774aa621
pdostal <git@pdostal.cz>
2014-12-23 11:13:42 +0100

    Create skeleton

--- page_uvod.php
+++ page_uvod.php
@@ -3,3 +3,21 @@
 Template Name: uvod
 */
 ?>
+<!doctype html>
+<html lang="cs">
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <meta name="author" content="goalball.cz">
+    <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS feed" href="<?php bloginfo('rss2_url'); ?>" />
+    <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/icon.png">
+    <!-- START WP_HEAD --><?php wp_head(); ?><!-- STOP WP_HEAD -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/uvod.css" media="all" />
+  </head>
+  <body>
+  </body>
+</html>
